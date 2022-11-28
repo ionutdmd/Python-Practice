2cf9be6108000f7bd9e8bc069d42f2da5f954187
ionutdmd <ionutdaus@gmail.com>
2022-11-28 23:06:14 +0200

    added 003 - Whats your name

---
+++ Hackerrank/03 - Strings/003 - Whats your name.py
@@ -0,0 +1,9 @@
+# https://www.hackerrank.com/challenges/whats-your-name/problem
+# Score: 10
+
+
+def print_full_name(first, last):
+    # Write your code here
+    print(f"Hello {first} {last}! You just delved into python.")
+
+
