37d70fba1a7fde382a74f3ebae44395e37380c3d
ionutdmd <ionutdaus@gmail.com>
2022-11-28 23:04:43 +0200

    added 002 - String Split and Join

---
+++ Hackerrank/03 - Strings/002 - String Split and Join.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/python-string-split-and-join/problem
+# Score: 10
+
+def split_and_join(line):
+    # write your code here
+    list = line.split(" ")
+
+    return "-".join(list)
