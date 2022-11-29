b7ac7e662e778bd42c6f37b8aca64fd48c80592f
ionutdmd <ionutdaus@gmail.com>
2022-11-29 16:41:32 +0200

    added CodeAbbey solutions

---
+++ CodeAbbey/001 - SumAB.py
@@ -0,0 +1,5 @@
+# https://www.codeabbey.com/index/task_view/sum-of-two
+
+valori = list(map(int, input().split(" ")))
+a, b = valori[0], valori[1]
+print(a + b)
---
+++ CodeAbbey/002 - Sum in loop.py
@@ -0,0 +1,5 @@
+# https://www.codeabbey.com/index/task_view/sum-in-loop
+
+n = int(input())
+valori = list(map(int,input().split(" ")))
+print(sum(valori))
